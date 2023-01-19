4b2d2f4484845efa577995448a76d610d5f3ebde
charlene <charlene.xu92@gmail.com>
2023-01-18 16:05:26 -0800

    Feature 9 complete

--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from todos.models import TodoList
+from todos.forms import TodoListForm
 
 # Create your views here.
 
 
+# Show all todolists
 def todo_list_list(request):
     todolists = TodoList.objects.all()
     context = {
@@ -12,9 +14,27 @@ def todo_list_list(request):
     return render(request, "todos/list.html", context)
 
 
+# Show detail of one of the todolists
 def todo_list_detail(request, id):
     todolist = TodoList.objects.get(id=id)
     context = {
         "todolist": todolist,
     }
     return render(request, "todos/detail.html", context)
+
+
+# Create view
+def todo_list_create(request):
+    if request.method == "POST":
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            todolist = form.save()
+            return redirect("todo_list_detail", id=todolist.id)
+    else:
+        form = TodoListForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "todos/create.html", context)
